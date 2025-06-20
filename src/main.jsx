7294f0f4329108d7ef324b5546f462ac0a27856c
chintha phanindra <chintha.phanindra40@gmail.com>
2025-06-20 14:58:05 +0530

    change

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Outlet } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
