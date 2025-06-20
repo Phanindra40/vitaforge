d66d77100786fd851da3d57487f0274eccfd5f16
chintha phanindra <chintha.phanindra40@gmail.com>
2025-06-20 15:36:37 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,7 @@ const AppRoutes = () => (
       path="/sign-up/*"
       element={
         <div className="flex justify-center items-center min-h-screen bg-white">
-          <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
+          <SignUp path="/sign-up/*" routing="path" signInUrl="/sign-in" />
         </div>
       }
     />
@@ -81,10 +81,16 @@ const AppRoutes = () => (
     <Route
       path="/dashboard"
       element={
-        <SignedIn>
-          <Dashboard />
-        </SignedIn>
-      }
+    <>
+      <SignedIn>
+        <Dashboard />
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </>
+  }
+
     />
 
     {/* Fallback route */}
