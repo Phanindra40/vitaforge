0a2213d0f30cdc91caf3444502c5b18af03a3dc0
chintha phanindra <chintha.phanindra40@gmail.com>
2025-06-21 18:32:25 +0530

    signup removed

--- src/App.jsx
+++ src/App.jsx
@@ -68,14 +68,7 @@ const AppRoutes = () => (
     />
 
     {/* Clerk Sign Up */}
-    <Route
-      path="/sign-up/*"
-      element={
-        <div className="flex justify-center items-center min-h-screen bg-white">
-          <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
-        </div>
-      }
-    />
+    
 
     {/* Protected Dashboard */}
     <Route
