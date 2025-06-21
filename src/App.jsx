65672c7d0a06f28cdbfed1c07ae6256eaf99aecb
chintha phanindra <chintha.phanindra40@gmail.com>
2025-06-21 18:43:01 +0530

    signup button

--- src/App.jsx
+++ src/App.jsx
@@ -69,14 +69,8 @@ const AppRoutes = () => (
 
     {/* Clerk Sign Up */}
     
-<Route
-  path="/sign-up/*"
-  element={
-    <div className="flex justify-center items-center min-h-screen bg-white">
-      <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
-    </div>
-  }
-/>
+<Route path="/sign-up/*" element={<SignUpPage />} />
+
 
 
 
